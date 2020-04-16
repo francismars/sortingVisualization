5c2e0308b8f33bbfaee6d8f80aaa795255dcb5b0
francismars <11526661+francismars@users.noreply.github.com>
2020-04-16 17:54:27 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const SELECTIONSORT = "<ul><li>Selection sort has an O(n^2) time complexity, whi
 const BUBBLESORT = "<ul><li>Bubble sort is a simple sorting algorithm that repeatedly steps through the list, compares adjacent elements and swaps them if they are in the wrong order. The pass through the list is repeated until the list is sorted. </li><li> Bubble sort has a worst-case and average complexity of O(n^2), where n is the number of items being sorted. Most practical sorting algorithms have substantially better worst-case or average complexity, often O(n log n). Even other O(n^2) sorting algorithms, such as insertion sort, generally run faster than bubble sort, and are no more complex. Therefore, bubble sort is not a practical sorting algorithm. </li></ul>"
 const INSERTIONSORT = "<ul><li>Insertion sort is a simple sorting algorithm that builds the final sorted or list one item at a time. It is much less efficient on large lists than more advanced algorithms such as quicksort, heapsort, or merge sort.</li><li> Insertion sort iterates, consuming one input element each repetition, and growing a sorted output list. At each iteration, insertion sort removes one element from the input data, finds the location it belongs within the sorted list, and inserts it there. It repeats until no input elements remain. </li><li> The best case input is an array that is already sorted. In this case insertion sort has a linear running time (O(n)). The simplest worst case input is an array sorted in reverse order.  This gives insertion sort a quadratic running time (O(n^2)). The average case is also quadratic, which makes insertion sort impractical for sorting large arrays. However, insertion sort is one of the fastest algorithms for sorting very small arrays, even faster than quicksort. </li></ul>"
 
-const LISTSIZE = 200
+const LISTSIZE = 50
 const WIDTH = 800
 const HEIGHT = 600
 let listIndex = 0
@@ -70,27 +70,35 @@ function bubbleSort(){
 }
 
 function insertionSort() {
+	if(volley==list.length){ listIndex=-1 }
 	fill('red')
-	rect((volley*(WIDTH/LISTSIZE)), HEIGHT, (WIDTH/LISTSIZE), -(list[volley]*HEIGHT/2))		
+	rect((volley*(WIDTH/LISTSIZE)), HEIGHT, (WIDTH/LISTSIZE), -(list[volley]*HEIGHT/2))	
+	fill('blue')
+	rect((listIndex*(WIDTH/LISTSIZE)), HEIGHT, (WIDTH/LISTSIZE), -(list[listIndex]*HEIGHT/2))		
+	if(listIndex<=volley){
+		if(list[volley]<=list[listIndex]){
+			list.splice(listIndex, 0, list[volley])
+			list.splice(volley+1, 1)
+			listIndex=0
+			volley++
+		} else { listIndex++ }
+		
+	}
+	if(listIndex==volley){
+		volley++
+		listIndex=0
+	}
 	for (k in list){
 		noFill()
 		rect((k*(WIDTH/LISTSIZE)), HEIGHT, (WIDTH/LISTSIZE), -(list[k]*HEIGHT/2))								
-	}
-	for(j=0;j<volley;j++){
-		if(list[volley]<=list[j]){
-			list.splice(j, 0, list[volley])
-			list.splice(volley+1, 1)
-			break
-		}
-	}
-	volley++
+	}	
 }
 
 function changeAlgorithm(){
 	let choice = sel.value()
 	if (choice=='Selection Sort'){
 		listIndex = 0
-		volley = 0
+		volley = 1
 		smaller = 0
 		algorithm="SelectionSort"
 		explanation.html(SELECTIONSORT)
@@ -98,7 +106,7 @@ function changeAlgorithm(){
 		
 	}
 	if (choice=='Bubble Sort'){
-		listIndex = -1
+		listIndex = 0
 		volley = 0
 		smaller = -1
 		algorithm="BubbleSort"
